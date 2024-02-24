85408091e5298edf655bfb0a302521aa0f36251a
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-02-24 14:08:11 +0700

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { app } from "./application/server.js";
 
 
-app.listen(3000, () => {
-    console.log(`Server start at port 3000`);
+app.listen(5000, () => {
+    console.log(`Server start at port 5000`);
 });
